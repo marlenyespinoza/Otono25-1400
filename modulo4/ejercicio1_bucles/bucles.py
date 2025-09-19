d7855dd473d14edb0e83630f3eb8d3e167f1bf80
Brandy Contreras <bcontrerasgodinez@mail.weber.edu>
2025-09-19 11:38:53 -0600

    Add files via upload

---
+++ modulo4/ejercicio1_bucles/bucles.py
@@ -0,0 +1,16 @@
+#TODO 1: Escriba un bucle (for loop) que imprima los números de 0 a 5 (inclusive).
+#Pista: Para que incluya el numero 5
+for i in range(6):
+    print (i)
+print () # Agregue un print () para crear una linea vacia o un espacio entre el bucle de numeros de 0-5 y el bucle de la palabra soy
+
+#TODO 2: Escriba un bucle for que imprima la frase "Soy " tres veces
+# Pista: Solo imprimir el texto no cambiarlo
+for i in range(3):
+    print ("soy")
+print () # Agregue un print () para crear una linea vacia o un espacio entre el bucle de la palabra soy y en bucle de numeros pares
+
+#TODO 3: Escriba un bucle (for loop) que imprima los números pares del 2 - 10 (inclusivo).
+#Pista: usa range() para contar de dos en dos.
+for numero in range(2, 11, 2):
+    print(numero)
